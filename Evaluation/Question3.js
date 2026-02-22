a0d7af26ab36403483cebc4332769169348331c5
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2026-02-22 16:57:15 +0530

    added q3

--- Evaluation/Question3.js
+++ Evaluation/Question3.js
@@ -1,3 +1,24 @@
 function mySetInterval(callback, delay){
-    
+    let id;
+    function repeat(){
+        callback();
+        id = setTimeout(repeat, delay);
+    }
+    id = setTimeout(repeat, delay);
+    return id;
 }
+
+function myClearInterval(id){
+    clearTimeout(id);
+}
+
+// let x = setTimeout(()=>{
+//     console.log(30);
+// }, 0)
+// console.log(x)
+
+let id = mySetInterval(()=>{console.log("Timer Running")}, 1000);
+
+setTimeout(()=>{
+    myClearInterval(id);
+}, 5000);
